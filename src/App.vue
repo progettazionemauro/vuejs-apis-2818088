6a64f4ff1fcdb6a723fa3a167ff939ba0a3ab79b
Mauro <40081963+progettazionemauro@users.noreply.github.com>
2020-05-03 10:10:24 +0200

    terza

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="container">
+
+<h1 class="text-primary mt-3"> {{name}} </H1>
+<p @click="showDescription=!showDescription" > <font-awsome-icon icon="play" /> <span v-if="showDescription"> {{description}} </span> </p>
+    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
+
+  data () {
+  return {
+showDescription:true,
+name: "Modern Hard Rock",
+description: "L'hard rock è una forma particolarmente grintosa e aggressiva di musica rock[1]. La chitarra elettrica viene spesso enfatizzata tramite la distorsione e altri effetti, sia durante l'uso come chitarra ritmica, sia come chitarra solista[1]"
   }
+  },
 };
 </script>
 
 <style lang="scss">
-#app {
+/*#app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
